6edec0bb08a2461bb9868c740c921667af3e2204
minsson <mskaang@gmail.com>
2022-10-30 14:12:24 +0900

    [Factory Pattern] 운송회사와 수단이 같은 형식으로 점점 확장되는 물류회사 앱
    - ProblemCode: 트럭킹 컴퍼니만 있을 거라고 생각하고 작성한 코드
        - 개방폐쇄 원칙을 정면으로 위반
        - 확장시 기존 클라이언트 코드를 갈아 엎어야 하는 구조
    - Concept: Creator, ConcreteCreator, Product, ConcreteProduct로 이루어진 구조
        - Creator 프로토콜은 Product 클래스의 새로운 인스턴스를 반환할 factory method를 선언한다.
        - Creator 프로토콜을 채택한 클래스들은 factory method에 대한 구현을 제공한다.
        - Creator는 factory method에 의해 리턴된 Product 객체에 기반한 코어 비즈니스 로직을 포함한다.
        - Creator를 채택한 타입들은 factory method를 오버라이드해 다른 타입의 product를 반환함으로써 비즈니스 로직을 간접적으로 바꾼다.
        - 클라이언트 코드에서는 설정이나 환경에 따라 원하는 ConcreteCreator 타입을 택해, 원하는 비즈니스 로직을 실행할 수 있다.
    - AppliedCode: 운송회사와 수단이 같은 형식으로 점점 확장되는 물류회사 앱을 가정하고 구현함
        - 드론, 퀵보드 등 새로운 운송회사와 해당 회사들이 이용하는 운송수단이 나와도 기존 비즈니스 로직 수정 없이 확장 가능하다.

---
+++ 3. Factory/Factory-Minsson/Factory-Minsson/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Factory-Minsson
+//
+//  Created by minsson on 2022/10/26.
+//
+
+import Foundation
+
+ForwarderCompany.orderDelivery(to: TruckingCompany())
+ForwarderCompany.orderDelivery(to: ShippingCompany())
+ForwarderCompany.orderDelivery(to: AirShippingCompany())
