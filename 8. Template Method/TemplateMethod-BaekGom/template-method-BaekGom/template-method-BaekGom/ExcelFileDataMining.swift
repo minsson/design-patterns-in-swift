0fc071321e35298c5aa9285a37a449177593a89a
BaekGom <jihunmm02@gmail.com>
2022-11-16 14:20:19 +0900

    feat: Template Method 패턴 예시

---
+++ 8. Template Method/TemplateMethod-BaekGom/template-method-BaekGom/template-method-BaekGom/ExcelFileDataMining.swift
@@ -0,0 +1,14 @@
+//
+//  ExcelFileDataMining.swift
+//  template-method-BaekGom
+//
+//  Created by Baek on 2022/11/09.
+//
+
+import Foundation
+
+class ExcelFileDataMining: DataMining {
+    override func getData() {
+        print("Excel File 데이터를 불러옵니다.")
+    }
+}
---
+++ 8. Template Method/TemplateMethod-BaekGom/template-method-BaekGom/template-method-BaekGom/PDFFileDataMining.swift
@@ -0,0 +1,14 @@
+//
+//  PDFFileDataMining.swift
+//  template-method-BaekGom
+//
+//  Created by Baek on 2022/11/09.
+//
+
+import Foundation
+
+class PDFFileDataMining: DataMining {
+    override func getData() {
+        print("PDF File 데이터를 불러옵니다.")
+    }
+}
