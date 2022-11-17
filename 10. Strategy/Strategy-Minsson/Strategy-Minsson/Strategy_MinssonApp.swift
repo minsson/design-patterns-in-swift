55e78c11f4af2654177090fcc592903e3a0c3226
minsson <mskaang@gmail.com>
2022-11-17 17:52:12 +0900

    [Strategy Pattern] SwiftUI로 초간단 내비게이션 이동수단 선택 UI 구현

---
+++ 10. Strategy/Strategy-Minsson/Strategy-Minsson/Strategy_MinssonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Strategy_MinssonApp.swift
+//  Strategy-Minsson
+//
+//  Created by minsson on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct Strategy_MinssonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
